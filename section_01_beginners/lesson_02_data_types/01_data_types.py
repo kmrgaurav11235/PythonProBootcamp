87cf7399161b75e3e28e655bbc1ab8214bf5d882
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-06-06 14:03:48 +0530

    Type conversion assignment + Refactoring

---
+++ section_01_beginners/lesson_02_data_types/01_data_types.py
@@ -0,0 +1,18 @@
+#Data Types
+
+# String
+print("Hello"[4])
+print("Hello"[-1])
+
+print("123" + "345")
+
+# Integer
+print(123 + 345)
+print(657_678_788)
+
+# Float
+print(4.78 + 1.23)
+
+# Boolean
+True
+False
--- section_01_beginners/lesson_02_data_types/02_type_conversion.py
+++ section_01_beginners/lesson_02_data_types/02_type_conversion.py
@@ -1,22 +1,3 @@
-#Data Types
-
-# String
-print("Hello"[4])
-print("Hello"[-1])
-
-print("123" + "345")
-
-# Integer
-print(123 + 345)
-print(657_678_788)
-
-# Float
-print(4.78 + 1.23)
-
-# Boolean
-True
-False
-
 num_char = len(input("Enter your name: "))
 print(num_char)
 
