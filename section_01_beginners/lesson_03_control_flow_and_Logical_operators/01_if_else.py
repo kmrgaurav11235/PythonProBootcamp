e5c6270ad4cae8ceefb1a242415bc6a6b81017b9
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-06-07 19:46:39 +0530

    Control Flow with if-else and conditional operators

---
+++ section_01_beginners/lesson_03_control_flow_and_Logical_operators/01_if_else.py
@@ -0,0 +1,20 @@
+# Control Flow
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+  print("You can ride the rollercoaster!")
+else:
+  print("Sorry, you have to grow taller before you ride.")
+
+# Write a program that works out whether if a given number is an odd or even number.
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
