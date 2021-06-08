439e96a99ae77c8c1f28ad7512266b66bece1520
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-06-08 22:20:54 +0530

    Nested if-else + elif

--- section_01_beginners/lesson_03_control_flow_and_Logical_operators/01_if_else.py
+++ section_01_beginners/lesson_03_control_flow_and_Logical_operators/01_if_else.py
@@ -4,6 +4,13 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
   print("You can ride the rollercoaster!")
+  age = int(input("What is your age? "))
+  if age < 12:
+    print("Please pay $5.")
+  elif age <= 18:
+    print("Please pay $7.")
+  else:
+    print("Please pay $12.")
 else:
   print("Sorry, you have to grow taller before you ride.")
 
@@ -17,4 +24,3 @@ if number % 2 == 0:
   print("This is an even number.")
 else:
   print("This is an odd number.")
-
