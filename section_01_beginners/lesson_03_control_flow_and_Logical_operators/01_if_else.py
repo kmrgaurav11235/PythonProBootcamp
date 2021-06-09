f84ccadb144ff21e29fc15de9b73970927aa36e7
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-06-09 23:36:05 +0530

    Multiple if-statements in a succession

--- section_01_beginners/lesson_03_control_flow_and_Logical_operators/01_if_else.py
+++ section_01_beginners/lesson_03_control_flow_and_Logical_operators/01_if_else.py
@@ -1,18 +1,28 @@
 # Control Flow
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
   print("You can ride the rollercoaster!")
   age = int(input("What is your age? "))
   if age < 12:
-    print("Please pay $5.")
+    bill = 5
+    print("Child tickets are $5.")
   elif age <= 18:
-    print("Please pay $7.")
+    bill = 7
+    print("Youth tickets are $7.")
   else:
-    print("Please pay $12.")
+    bill = 12
+    print("Adult tickets are $12.")
 else:
   print("Sorry, you have to grow taller before you ride.")
+
+wants_photo = input("Do you want a photo taken? Y or N. ")
+if wants_photo == "Y":
+  bill += 3
+
+print(f"Your final bill is ${bill}")
 
 # Write a program that works out whether if a given number is an odd or even number.
 # 🚨 Don't change the code below 👇
