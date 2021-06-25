e3c4b5bd9d3b5acee15b7eb12938f37a0557fa3e
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-06-25 21:52:58 +0530

    Grading Program: end

--- section_02_beginners/lesson_02_dictionaries_and_nesting/02_grading_program.py
+++ section_02_beginners/lesson_02_dictionaries_and_nesting/02_grading_program.py
@@ -1,7 +1,7 @@
-# You have access to a database of student_scores in the format of a dictionary. The keys in 
+# You have access to a database of student_scores in the format of a dictionary. The keys in
 # student_scores are the names of the students and the values are their exam scores.
-# Write a program that converts their scores to grades. By the end of your program, you should 
-# have a new dictionary called student_grades that should contain student names for keys and 
+# Write a program that converts their scores to grades. By the end of your program, you should
+# have a new dictionary called student_grades that should contain student names for keys and
 # their grades for values. The final version of the student_grades dictionary will be checked.
 # This is the scoring criteria:
 # Scores 91 - 100: Grade = "Outstanding"
@@ -13,25 +13,29 @@
 # Hint
 # Remember that looping through a Dictionary will only give you the keys and not the values.
 student_scores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99, 
-  "Draco": 74,
-  "Neville": 62,
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
 }
 # 🚨 Don't change the code above 👆
 
-#TODO-1: Create an empty dictionary called student_grades.
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
 
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for name in student_scores:
+    score = student_scores[name]
+    if score > 90 and score <= 100:
+        student_grades[name] = "Outstanding"
+    elif score > 80 and score <= 90:
+        student_grades[name] = "Exceeds Expectations"
+    elif score > 70 and score <= 80:
+        student_grades[name] = "Acceptable"
+    elif score <= 70:
+        student_grades[name] = "Fail"
 
-#TODO-2: Write your code below to add the grades to student_grades.👇
-
-    
 
 # 🚨 Don't change the code below 👇
 print(student_grades)
-
-
-
-
-
