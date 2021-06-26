1451f61a339782f0f795e7b3e05e53613fa8aefb
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-06-26 19:55:27 +0530

    Multiple return values

--- section_02_beginners/lesson_03_functions_with_outputs/01_functions_with_outputs.py
+++ section_02_beginners/lesson_03_functions_with_outputs/01_functions_with_outputs.py
@@ -1,9 +1,16 @@
 # Functions with outputs
 def format_name(f_name, l_name):
+    if f_name == "" and l_name == "":
+        return "You didn't provide valid inputs"
+        # Multiple returns are allowed in the same method
+        # return with nothing following it -> Returns a 'None'
+
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
 
     return f"{formatted_f_name} {formatted_l_name}"
+    # This print statement will never get executed as it comes after return
+    print("This is unreachable")
 
 
 formatted_string = format_name("eLu", "ThiNgOl")
