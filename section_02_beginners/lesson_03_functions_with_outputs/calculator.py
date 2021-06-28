80bbed23acd6d8ad6095757f18800bec5a626fbb
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-06-28 23:58:57 +0530

    Recursive call to start the caluclator again

--- section_02_beginners/lesson_03_functions_with_outputs/calculator.py
+++ section_02_beginners/lesson_03_functions_with_outputs/calculator.py
@@ -20,18 +20,23 @@ operations = {
     "*": multiply,
     "/": divide
 }
-continue_calculating = "y"
-num1 = int(input("What is the first number?: "))
-for symbol in operations:
-    print(symbol)
-while continue_calculating.lower() == "y":
-    operation_symbol = input("Pick an operation: ")
-    num2 = int(input("What is the next number?: "))
 
-    answer = operations[operation_symbol](num1, num2)
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
-    num1 = answer
+def calculator():
+    continue_calculating = "y"
+    num1 = int(input("What is the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    while continue_calculating.lower() == "y":
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What is the next number?: "))
 
-    continue_calculating = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        num1 = answer
+
+        continue_calculating = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ")
+    else:
+        calculator()
 
         
+calculator()
