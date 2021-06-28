c6186bc2f819bca123e52f6eb4ea2d5674bad82f
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-06-29 00:08:26 +0530

    Finishing touches and bug fixes

--- section_02_beginners/lesson_03_functions_with_outputs/calculator.py
+++ section_02_beginners/lesson_03_functions_with_outputs/calculator.py
@@ -1,3 +1,4 @@
+from art import logo
 # Add
 def add(n1, n2):
     return n1 + n2
@@ -22,21 +23,24 @@ operations = {
 }
 
 def calculator():
+    print(logo)
     continue_calculating = "y"
-    num1 = int(input("What is the first number?: "))
+    num1 = float(input("What is the first number?: "))
     for symbol in operations:
         print(symbol)
     while continue_calculating.lower() == "y":
         operation_symbol = input("Pick an operation: ")
-        num2 = int(input("What is the next number?: "))
+        num2 = float(input("What is the next number?: "))
 
         answer = operations[operation_symbol](num1, num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}")
         num1 = answer
 
-        continue_calculating = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ")
+        continue_calculating = input(f"Type 'y' to continue calculating with {answer}, type 'n' to" 
+            + " start a new calculation, or type 'x' to exit.: ")
     else:
-        calculator()
+        if continue_calculating.lower() == 'n':
+            calculator()
 
         
 calculator()
