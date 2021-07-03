5046c804c5ab55a17e08f853d26fffcf430cb595
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-07-03 17:46:20 +0530

    Python doesn't have Block scope

--- section_02_beginners/lesson_05_scope_and_number_guessing_game/01_local_vs_global_scope.py
+++ section_02_beginners/lesson_05_scope_and_number_guessing_game/01_local_vs_global_scope.py
@@ -1,13 +1,13 @@
 ################### Scope ####################
 
-# Local Scope
+# Local Scope: Inside a function
 def drink_potion0():
     potion_strength = 2
     print(potion_strength) # ok
 
 # print(potion_strength) # Gives NameError: name 'potion' is not defined
 
-# Global Scope
+# Global Scope: Outside function
 player_health = 10
 def drink_potion1():
     print(player_health) # ok
@@ -27,3 +27,15 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}") # Prints 1
+
+# Python does not have "Block Scope"
+game_level = 3
+enemies = ["Skeleton", "Zombie", "Aliens"]
+if game_level < 5:
+  new_enemy = enemies[0]
+
+print(new_enemy) # This is valid even though it was created inside the "if-condition". Same for loops.
+# But if we put the creation of new_enemy inside a function, this will not work
+# Thus, loops and conditions don't create a separate local scope. Only functions do.
+
+
