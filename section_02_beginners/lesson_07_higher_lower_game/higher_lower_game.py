604662a3c77ca1d1f6d46736770623843e415af0
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-07-05 20:41:59 +0530

    Higher-Lower game: 2

--- section_02_beginners/lesson_07_higher_lower_game/higher_lower_game.py
+++ section_02_beginners/lesson_07_higher_lower_game/higher_lower_game.py
@@ -6,36 +6,42 @@ from random import choice
 # Initialize score
 score = 0
 
-# Fetch data from list and Pick two random data
-entity1 = choice(data)
-entity2 = choice(data)
-
-# Store the correct choice
-if entity1['follower_count'] > entity2['follower_count']:
-    correct_choice_entity = entity1
-else:
-    correct_choice_entity = entity2
-print(f"Correct choice: {correct_choice_entity}")
-
-# Display 1 vs 2
+continue_game = True
+first_entity = choice(data)
 print(logo)
-print(f"Compare A: {entity1['name']}, a {entity1['description']} from {entity1['country']}")
-print(vs)
-print(f"Against B: {entity2['name']}, a {entity2['description']} from {entity2['country']}")
-# Get user input
-user_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
 
-# Check if user is right - if yes loop, if no show end message
-if user_choice == "A":
-    user_choice_entity = entity1
-elif user_choice == "B":
-    user_choice_entity = entity2
-else:
-    print("Wrong Choice")
+while continue_game:
+    # Fetch data from list and Pick two random data
+    second_entity = choice(data)
 
-if user_choice_entity == correct_choice_entity:
-    score += 1
-    print(f"You're right! Current score: {score}.")
-else:
+    # Store the correct choice
+    if first_entity['follower_count'] > second_entity['follower_count']:
+        correct_choice_entity = first_entity
+    else:
+        correct_choice_entity = second_entity
+    print(f"Correct choice: {correct_choice_entity}")
+
+    # Display 1 vs 2
+    print(f"Compare A: {first_entity['name']}, a {first_entity['description']} from {first_entity['country']}")
+    print(vs)
+    print(f"Against B: {second_entity['name']}, a {second_entity['description']} from {second_entity['country']}")
+    # Get user input
+    user_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+    # Check if user is right - if yes loop, if no show end message
+    if user_choice == "A":
+        user_choice_entity = first_entity
+    elif user_choice == "B":
+        user_choice_entity = second_entity
+    else:
+        print("Invalid Choice!")
+        continue_game = False
+
     print(logo)
-    print(f"Sorry, that's wrong. Final score: {score}.")
+    if user_choice_entity == correct_choice_entity:
+        score += 1
+        print(f"You're right! Current score: {score}.")
+        first_entity = second_entity
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}.")
+        continue_game = False
