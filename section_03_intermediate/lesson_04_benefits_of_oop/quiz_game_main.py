b7d128a297ddb14ae5093d1226ddf8e70b0a6b2f
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-07-15 23:13:19 +0530

    Quiz Game 3: QuizBrain class

--- section_03_intermediate/lesson_04_benefits_of_oop/quiz_game_main.py
+++ section_03_intermediate/lesson_04_benefits_of_oop/quiz_game_main.py
@@ -1,10 +1,11 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for data in question_data:
     question = Question(data["text"], data["answer"])
     question_bank.append(question)
 
-for question in question_bank:
-    print(f"Q: {question.text}\n A: {question.answer}")
+quiz_brain = QuizBrain(question_bank)
+quiz_brain.next_question()
