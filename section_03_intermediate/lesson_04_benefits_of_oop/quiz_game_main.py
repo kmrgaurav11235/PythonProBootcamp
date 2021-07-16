b4d866408a9dad8a660dcce004302b91f2076eda
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-07-16 09:59:24 +0530

    Quiz Game 5: Checking answer and keeping score

--- section_03_intermediate/lesson_04_benefits_of_oop/quiz_game_main.py
+++ section_03_intermediate/lesson_04_benefits_of_oop/quiz_game_main.py
@@ -11,3 +11,5 @@ quiz_brain = QuizBrain(question_bank)
 while quiz_brain.still_has_questions():
     quiz_brain.next_question()
 
+print("You've completed the Quiz.")
+print(f"Your final score was {quiz_brain.score}/{quiz_brain.question_number}")
