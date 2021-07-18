f74d6b8ccf708995be3885e667c4cad697c8b6ad
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-07-18 18:37:02 +0530

    Generate a random walk

---
+++ section_03_intermediate/lesson_05_turtle_and_gui/04_generate_random_walk.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+from random import randint, choice
+
+tim = Turtle()
+tim.pensize(10)
+tim.speed("fastest")
+screen = Screen()
+screen.colormode(255)
+directions = [0, 90, 180, 270]
+
+for _ in range(200):
+    random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
+    tim.color(random_color, random_color)
+    random_direction = choice(directions)
+    tim.setheading(random_direction)
+    tim.forward(50)
+
+screen.exitonclick()
