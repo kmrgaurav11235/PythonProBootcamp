8afb0f727c9fb16419f53f6bafa346cef1d707c0
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-08-05 21:23:57 +0530

    List Comprehension: exercises

---
+++ section_04_intermediate/lesson_05_list_comprehension/02_list_comprehension_excercises.py
@@ -0,0 +1,41 @@
+# ----------------- Question 1 -----------------
+# You are going to write a List Comprehension to create a new list called squared_numbers. This new list should contain
+# every number in the list numbers but each number should be squared.
+# e.g. 4 * 4 = 16
+# 4 squared equals 16.
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above 👆
+
+# Write your 1 line code 👇 below:
+
+
+
+# Write your code 👆 above:
+
+print(squared_numbers)
+
+# ----------------- Question 2 -----------------
+# You are going to write a List Comprehension to create a new list called result. This new list should only contain the
+# even numbers from the list numbers.
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above
+
+# Write your 1 line code 👇 below:
+
+
+
+# Write your code 👆 above:
+
+print(result)
+
+# ----------------- Question 3 -----------------
+# Take a look inside file1.txt and file2.txt. They each contain a bunch of numbers, each number on a new line.
+# You are going to create a list called result which contains the numbers that are common in both files.
+# IMPORTANT: The result should be a list that contains Integers, not Strings.
+
+
+
+# Write your code above 👆
+
+print(result)
+
