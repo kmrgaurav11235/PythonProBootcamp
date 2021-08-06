75e3ba5f3c35fa0580ccffb981f4c879f5bfa14d
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-08-06 23:50:05 +0530

    Dict Comprehension: exercises

---
+++ section_04_intermediate/lesson_05_list_comprehension/04_dictionary_comprehension_exercises.py
@@ -0,0 +1,33 @@
+# You are going to use Dictionary Comprehension to create a dictionary called result that takes each word in the
+# given sentence and calculates the number of letters in each word.
+# Try Googling to find out how to convert a sentence into a list of words.
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+
+# Write your code below:
+
+
+
+print(result)
+
+# You are going to use Dictionary Comprehension to create a dictionary called weather_f that takes each temperature in
+# degrees Celsius and converts it into degrees Fahrenheit.
+# To convert temp_c into temp_f:
+# (temp_c * 9/5) + 32 = temp_f
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+
+# Write your code 👇 below:
+
+
+print(weather_f)
