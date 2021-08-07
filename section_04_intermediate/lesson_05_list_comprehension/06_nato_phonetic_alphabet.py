55a9fcfba55ce0e7b8d3dba1c82df342b0d5c5ea
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-08-07 15:56:35 +0530

    Nato phoentic alphabet: solution

--- section_04_intermediate/lesson_05_list_comprehension/06_nato_phonetic_alphabet.py
+++ section_04_intermediate/lesson_05_list_comprehension/06_nato_phonetic_alphabet.py
@@ -1,5 +1,13 @@
-# TODO 1. Create a dictionary in this format:
+import pandas
+
+# 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
+nato_ph_alphabet_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_ph_alphabet_dict = {row.letter: row.code for (index, row) in nato_ph_alphabet_data_frame.iterrows()}
+# print(nato_ph_alphabet_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# 2. Create a list of the phonetic code words from a word that the user inputs.
+input_word = input("Enter your word: ").upper()
+output_list = [nato_ph_alphabet_dict.get(letter) for letter in input_word]
 
+print(output_list)
