eb62e696851e87ab064b5762ea3ddac29573fb8e
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-08-26 22:42:48 +0530

    Password Manager: saving data to file

--- section_04_intermediate/lesson_08_password_manager/password_manager.py
+++ section_04_intermediate/lesson_08_password_manager/password_manager.py
@@ -1,7 +1,27 @@
 from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+DATA_FILE_NAME = "data.txt"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save_data():
+    # Get data
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+    data = f"{website} | {username} | {password}\n"
+
+    # Save the data
+    with open(file=DATA_FILE_NAME, mode="a") as data_file:
+        data_file.write(data)
+
+    # Clear widgets
+    website_entry.delete(first=0, last=END)
+    password_entry.delete(first=0, last=END)
+    website_entry.focus()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -27,11 +47,13 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # website entry
-website_entry = Entry(width=35)
+website_entry = Entry(width=38)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()  # Put the cursor here
 
 # username entry
-username_entry = Entry(width=35)
+username_entry = Entry(width=38)
+username_entry.insert(index=0, string="kgaurav@email.com")  # Insert default text at the begining
 username_entry.grid(row=2, column=1, columnspan=2)
 
 # password entry
@@ -43,7 +65,7 @@ generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
 
 # Add button
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save_data)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
