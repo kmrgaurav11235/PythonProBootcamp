2d7acff4ac5f6e992a554ec24c90ac7be0c14cc2
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-08-30 22:25:28 +0530

    Nato Phonetic Alphabet: Add exception handling

--- section_05_intermediate/lesson_01_errors_and_exceptions/04_nato_phonetic_alphabet.py
+++ section_05_intermediate/lesson_01_errors_and_exceptions/04_nato_phonetic_alphabet.py
@@ -6,8 +6,15 @@ nato_ph_alphabet_data_frame = pandas.read_csv("04_nato_phonetic_alphabet.csv")
 nato_ph_alphabet_dict = {row.letter: row.code for (index, row) in nato_ph_alphabet_data_frame.iterrows()}
 # print(nato_ph_alphabet_dict)
 
-# 2. Create a list of the phonetic code words from a word that the user inputs.
-input_word = input("Enter your word: ").upper()
-output_list = [nato_ph_alphabet_dict.get(letter) for letter in input_word]
+is_input_valid = False
 
-print(output_list)
+while not is_input_valid:
+    try:
+        # 2. Create a list of the phonetic code words from a word that the user inputs.
+        input_word = input("Enter your word: ").upper()
+        output_list = [nato_ph_alphabet_dict[letter] for letter in input_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(output_list)
+        is_input_valid = True
