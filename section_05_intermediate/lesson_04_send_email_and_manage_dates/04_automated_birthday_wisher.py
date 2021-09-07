3ed7feac00fd4789f719135b6f4b12dd36f35c7f
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-09-07 22:17:41 +0530

    Automated birthday wisher: complete

--- section_05_intermediate/lesson_04_send_email_and_manage_dates/04_automated_birthday_wisher.py
+++ section_05_intermediate/lesson_04_send_email_and_manage_dates/04_automated_birthday_wisher.py
@@ -1,8 +1,47 @@
-# 1. Update the birthdays.csv
+import pandas
+import random
+import smtplib
+import datetime as dt
 
-# 2. Check if today matches a birthday in the birthdays.csv
+BIRTHDAYS_CSV_FILE_PATH = "birthdays.csv"
+NAME_PLACEHOLDER = "[NAME]"
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual
-# name from birthdays.csv
 
-# 4. Send the letter generated in step 3 to that person's email address.
+def is_today(month, day):
+    """Check if today matches the provided month and date"""
+    today = dt.datetime.today()
+    return (month == today.month) and (day == today.day)
+
+
+def get_letter_body(name):
+    """Pick a random letter from letter templates and replace the [NAME] with the provided name"""
+    file_index = random.randint(1, 3)
+    chosen_letter_path = random.choice(f"04_letter_templates/letter_{file_index}.txt")
+    with open(file=chosen_letter_path, mode="r") as letter_file:
+        letter_text = letter_file.read()
+    return letter_text.replace(NAME_PLACEHOLDER, name)
+
+
+def send_email(email_address, email_body):
+    """Send email to the provided email_address with the provided email_body"""
+    my_email = "my_email@gmail.com"  # Update email
+    password = "my_password"  # Update password
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=email_address,
+            msg=f"Subject:Happy Birthday!\n\n{email_body}"
+        )
+
+
+birthday_data_frame = pandas.read_csv(filepath_or_buffer=BIRTHDAYS_CSV_FILE_PATH)
+
+for _, data in birthday_data_frame.iterrows():
+    if is_today(data["month"], data["day"]):
+        letter_body = get_letter_body(data["name"])
+        # print(data["email"], letter_body)
+        send_email(data["email"], letter_body)
+
